a121b1e587f9d73db6d0a3bddcf86f7316806863
SARANYA251096 <saranyaayyamuthu96@gmail.com>
2023-05-12 16:02:42 +0530

    updated

--- js/script.js
+++ js/script.js
@@ -1,15 +1,13 @@
 function sendEmail() {
-    Email.send({
-      SecureToken: "9d9e5372-66a0-4781-96f3-5a8438461541",
-      To: "vizhi121@gmail.com",
-      From: document.getElementById("email").value,
-      Subject: "New contact form enquiry",
-      Body:
-        "Name: " +
-        document.getElementById("name").value +
-        "<br> Email: " +
-        document.getElementById("email").value +
-        "<br> Message: " +
-        document.getElementById("message").value,
-    }).then((message) => alert("Message sent successfully!!!"));
+  var params = {
+    from_name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  emailjs
+    .send("service_zpe4bup", "template_ecq5gyi", params)
+    .then(function (res) {
+      alert("Messsage sent successfully!!!");
+    });
 }
